e11768fc7cd6201b30c390e071db22538fcbb69a
ayanhil <95565294+ayanhil@users.noreply.github.com>
2026-07-28 11:03:04 +0100

    fix(security): ajouter blob: au script-src CSP pour Firebase Auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ function applySecurityHeaders(response: NextResponse, isHttps: boolean): NextRes
     'Content-Security-Policy',
     [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.paypal.com https://*.firebaseio.com https://*.googleapis.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://*.paypal.com https://*.firebaseio.com https://*.googleapis.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: blob: https:",
       "font-src 'self' data: https://fonts.gstatic.com",
